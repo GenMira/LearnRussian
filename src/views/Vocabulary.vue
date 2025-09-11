f3149516b33fa8eba0f2f10db63307ba82a8364b
{genMira} <{weilianzhuantongyin@gmail.com}>
2025-09-12 06:32:39 +0900

    パス修正

--- src/views/Vocabulary.vue
+++ src/views/Vocabulary.vue
@@ -5,28 +5,28 @@
   </div>
   <div class="buttons">
     <div class="button-grid">
-      <router-link to="/vocabulary/noun"class="category-button">
+      <router-link to="/LearnRussian/vocabulary/noun"class="category-button">
         <!-- <img src="/src/assets/letter.jpg" alt="Letter" class="button-image" /> -->
         <span class="button-text">名詞</span>
       </router-link>
 
-      <router-link to="/vocabulary/verb" class="category-button">
+      <router-link to="/LearnRussian/vocabulary/verb" class="category-button">
         <!-- <img src="/src/assets/grammar.jpg" alt="Grammar" class="button-image" /> -->
         <span class="button-text">動詞</span>
       </router-link>
 
-      <router-link to="/vocabulary/adjective" class="category-button">
+      <router-link to="/LearnRussian/vocabulary/adjective" class="category-button">
         <!-- <img src="/src/assets/vocabulary.jpg" alt="Vocabulary" class="button-image" /> -->
         <span class="button-text">形容詞</span>
       </router-link>
     </div>
     <div class="button-grid">
-      <router-link to="/vocabulary/preposition"class="category-button">
+      <router-link to="/LearnRussian/vocabulary/preposition"class="category-button">
         <!-- <img src="/src/assets/letter.jpg" alt="Letter" class="button-image" /> -->
         <span class="button-text">前置詞</span>
       </router-link>
 
-      <router-link to="/vocabulary/pronoun"class="category-button">
+      <router-link to="/LearnRussian/vocabulary/pronoun"class="category-button">
         <!-- <img src="/src/assets/letter.jpg" alt="Letter" class="button-image" /> -->
         <span class="button-text">代名詞、疑問詞</span>
       </router-link>
