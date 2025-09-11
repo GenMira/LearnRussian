ab361bc24d6531a54efb81ea1772a5cc26212305
{genMira} <{weilianzhuantongyin@gmail.com}>
2025-09-12 04:46:21 +0900

    公開用設定変更

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/LearnRussian/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
