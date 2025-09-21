21e62231756838aeb97c57717afb155b4fd9da66
{genMira} <{weilianzhuantongyin@gmail.com}>
2025-09-21 16:59:58 +0900

    男性名詞まで

--- src/views/Declension.vue
+++ src/views/Declension.vue
@@ -5,16 +5,226 @@
   </div>
   <div class="text-box">
     <h2>人称代名詞</h2>
-    <!-- <p>固有名詞もここに</p> -->
+    <br>
+    <table class="center-table">
+      <tbody>
+        <tr>
+          <td>主格<br>(Именительный)</td>
+          <td>я</td>
+          <td>ты</td>
+          <td>он / оно</td>
+          <td>она</td>
+          <td>мы</td>
+          <td>вы</td>
+          <td>они</td>
+        </tr>
+        <tr>
+          <td>生格<br>(Родительный)</td>
+          <td>меня́</td>
+          <td>тебя́</td>
+          <td>его́</td>
+          <td>её</td>
+          <td>нас</td>
+          <td>вас</td>
+          <td>их</td>
+        </tr>
+        <tr>
+          <td>与格<br>(Дательный)</td>
+          <td>мне́</td>
+          <td>тебе́</td>
+          <td>ему́</td>
+          <td>ей</td>
+          <td>нам</td>
+          <td>вам</td>
+          <td>им</td>
+        </tr>
+        <tr>
+          <td>対格<br>(Винительный)</td>
+          <td>меня́</td>
+          <td>тебя́</td>
+          <td>его́</td>
+          <td>её</td>
+          <td>нас</td>
+          <td>вас</td>
+          <td>их</td>
+        </tr>
+        <tr>
+          <td>造格<br>(Творительный)</td>
+          <td>мной</td>
+          <td>тобо́й</td>
+          <td>им</td>
+          <td>ей (ею)</td>
+          <td>нами</td>
+          <td>вами</td>
+          <td>ими</td>
+        </tr>
+        <tr>
+          <td>前置格<br>(Предложный)</td>
+          <td>мне́</td>
+          <td>тебе́</td>
+          <td>нём</td>
+          <td>ней</td>
+          <td>нас</td>
+          <td>вас</td>
+          <td>них</td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <p>三人称の人称代名詞は、主格以外の形で、前置詞の後ろに来たときに「н-」が付く</p>
+    <br>
+    <p>Я иду́ с ним</p>
+    <p>私は彼と一緒に行きます</p>
   </div>
   <div class="text-box">
     <h2>疑問詞</h2>
-  </div>
-  <div class="text-box">
-    <h2>動詞</h2>
+    <br>
+    <table class="center-table">
+      <thead>
+        <tr>
+          <th>格</th>
+          <th>кто<br>(誰)</th>
+          <th>что<br>(何)</th>
+          <th>како́й<br>(どんな)</th>
+          <th>че́й<br>(誰の)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>主格<br>(Именительный)</td>
+          <td>кто</td>
+          <td>что</td>
+          <td>како́й</td>
+          <td>чей</td>
+        </tr>
+        <tr>
+          <td>生格<br>(Родительный)</td>
+          <td>кого́</td>
+          <td>чего́</td>
+          <td>како́го</td>
+          <td>чьего́</td>
+        </tr>
+        <tr>
+          <td>与格<br>(Дательный)</td>
+          <td>кому́</td>
+          <td>чему́</td>
+          <td>како́му</td>
+          <td>че́йму / чьему́</td>
+        </tr>
+        <tr>
+          <td>対格<br>(Винительный)</td>
+          <td>кого́</td>
+          <td>что</td>
+          <td>како́го</td>
+          <td>чьего́</td>
+        </tr>
+        <tr>
+          <td>造格<br>(Творительный)</td>
+          <td>кем</td>
+          <td>чем</td>
+          <td>каки́м</td>
+          <td>чи́м / чьим</td>
+        </tr>
+        <tr>
+          <td>前置格<br>(Предложный)</td>
+          <td>о ком</td>
+          <td>о чём</td>
+          <td>о како́м</td>
+          <td>о чьём</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
   <div class="text-box">
     <h2>名詞</h2>
+    <br>
+    <h3>男性名詞</h3>
+    <br>
+    <table class="center-table">
+      <tbody>
+        <tr>
+          <td rowspan="6">単数</td>
+          <td>主</td>
+          <td>журна́л</td>
+          <td>музе́й</td>
+          <td>слова́рь</td>
+        </tr>
+        <tr>
+          <td>属</td>
+          <td>журна́ла</td>
+          <td>музе́я</td>
+          <td>слова́ря</td>
+        </tr>
+        <tr>
+          <td>与</td>
+          <td>журна́лу</td>
+          <td>музе́ю</td>
+          <td>слова́рю</td>
+        </tr>
+        <tr>
+          <td>対</td>
+          <td>журна́л</td>
+          <td>музе́й</td>
+          <td>слова́рь</td>
+        </tr>
+        <tr>
+          <td>造</td>
+          <td>журна́лом</td>
+          <td>музе́ем</td>
+          <td>слова́рем</td>
+        </tr>
+        <tr>
+          <td>前</td>
+          <td>о журна́ле</td>
+          <td>о музе́е</td>
+          <td>о слова́ре</td>
+        </tr>
+        <tr>
+          <td rowspan="6">複数</td>
+          <td>主</td>
+          <td>журна́лы</td>
+          <td>музе́и</td>
+          <td>слова́ри</td>
+        </tr>
+        <tr>
+          <td>属</td>
+          <td>журна́лов</td>
+          <td>музе́ев</td>
+          <td>слова́рей</td>
+        </tr>
+        <tr>
+          <td>与</td>
+          <td>журна́лам</td>
+          <td>музе́ям</td>
+          <td>слова́рям</td>
+        </tr>
+        <tr>
+          <td>対</td>
+          <td>журна́лы</td>
+          <td>музе́и</td>
+          <td>слова́ри</td>
+        </tr>
+        <tr>
+          <td>造</td>
+          <td>журна́лами</td>
+          <td>музе́ями</td>
+          <td>слова́рями</td>
+        </tr>
+        <tr>
+          <td>前</td>
+          <td>о журна́лах</td>
+          <td>о музе́ях</td>
+          <td>о слова́рях</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <p>固有名詞もここに</p> -->
+
+
+
+  </div>
+  <div class="text-box">
+    <h2>動詞</h2>
   </div>
   <div class="text-box">
     <h2>形容詞</h2>
