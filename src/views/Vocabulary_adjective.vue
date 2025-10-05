8348478923e8117425fd82077a07469c8039425d
{genMira} <{weilianzhuantongyin@gmail.com}>
2025-10-05 15:40:32 +0900

    形容詞

--- src/views/Vocabulary_adjective.vue
+++ src/views/Vocabulary_adjective.vue
@@ -8,4 +8,61 @@
     </router-link>
     <br>
   </div>
+  <div class="text-box">
+    <h2>よく使う</h2>
+    <br>
+    <table class="center-table">
+      <caption><b>ロシア語の形容詞一覧</b></caption>
+      <thead>
+        <tr>
+          <th>語句</th>
+          <th>意味</th>
+          <th>備考</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>но́вый</td>
+          <td>新しい</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>ста́рый</td>
+          <td>古い</td>
+          <td>比較級は ста́рше（年上という意味でも使う）</td>
+        </tr>
+        <tr>
+          <td>большо́й</td>
+          <td>大きい</td>
+          <td>不規則な比較級: бо́льше</td>
+        </tr>
+        <tr>
+          <td>ма́ленький</td>
+          <td>小さい</td>
+          <td>指小形（малый の派生）</td>
+        </tr>
+        <tr>
+          <td>хоро́ший</td>
+          <td>良い</td>
+          <td>比較級: лу́чший（不規則変化）</td>
+        </tr>
+        <tr>
+          <td>плохо́й</td>
+          <td>悪い</td>
+          <td>比較級: ху́же（不規則変化）</td>
+        </tr>
+        <tr>
+          <td>ра́нний</td>
+          <td>早い（時間的に）</td>
+          <td>「й」で終わる特殊な形容詞</td>
+        </tr>
+        <tr>
+          <td>дорого́й</td>
+          <td>高価な／親愛なる</td>
+          <td>「手紙の挨拶」で「親愛なる〜」の意味でも使う</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
 </template>
